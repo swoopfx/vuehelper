eff03fd104f60750f68f20d2774afae8f181dcc4
mac <ezekiel_a@yahoo.com>
2021-09-23 19:50:59 +0100

    fixed composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2e8d1f7bfab96c511678ba4ae7500efed4729333',
+        'name' => 'swoopfx/vuehelper',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'swoopfx/vuehelper' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2e8d1f7bfab96c511678ba4ae7500efed4729333',
+            'dev_requirement' => false,
+        ),
+    ),
+);
